4dc133b575770b89c36914830776c7c9a0bef0c9
Ryotaro Kusumoto <kusumobile@icloud.com>
2019-06-26 16:01:48 +0900

    added show commit success result page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -14,14 +18,23 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
-
   end
 
   private
     def article_params
       params.require(:article).permit(:title, :description)
     end
-    
+
 end
