55cbb506afbe90e15a324c386b6fc018471e1ac6
Ryotaro Kusumoto <kusumobile@icloud.com>
2019-06-27 17:11:44 +0900

    password_digest added

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'bootstrap-sass', '~> 3.4.1'
 gem 'sassc-rails', '>= 2.1.0'
 gem 'jquery-rails'
 
+gem 'bcrypt', '~> 3.1.13'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
---
+++ db/users.sql
@@ -0,0 +1,7 @@
+insert into users (id, username, email, created_at, updated_at)
+values
+(1,'Taro','taro@abc.com','2019-06-01 00:00:00','2019-06-01 00:00:00'),
+(2,'Jiro','jiro@abc.com','2019-06-01 00:00:00','2019-06-01 00:00:00'),
+(3,'Saburo','saburo@abc.com','2019-06-01 00:00:00','2019-06-01 00:00:00'),
+(4,'Shiro','shiro@abc.com','2019-06-01 00:00:00','2019-06-01 00:00:00'),
+(5,'Goro','goro@abc.com','2019-06-01 00:00:00','2019-06-01 00:00:00');
